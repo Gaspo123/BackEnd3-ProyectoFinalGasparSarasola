4f752f5c57a8912adf854951619e562a7e713374
Gaspar <gaspox.lt@gmail.com>
2025-03-26 19:39:24 -0300

    errorHandler

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -5,60 +5,63 @@ import {
   createMocks,
   read,
 } from "../services/products.service.js";
+import CustomError from "../utils/errors/custom.error.js";
 import { notFound } from "../utils/errors/dictionary.error.js";
 
-const createProduct = async (req, res) => {
+const createProduct = async (req, res, next) => {
   try {
     const data = req.body;
     const one = await create(data);
     return res.status(201).json({ message: "Created!", response: one });
   } catch (error) {
-    return res.status(500).json({ error });
+    next(error);
   }
 };
 
-const readOneProduct = async (req, res) => {
+const readOneProduct = async (req, res, next) => {
   try {
     const { pid } = req.params;
     const one = await Product.findById(pid);
     if (one) {
-      return res.stauts(500).json({ message: "Read!", response: one });
+      return res.stauts(200).json({ message: "Read!", response: one });
     } else {
-      return res
-        .status(notFound.statusCode)
-        .json({ message: notFound.message });
+      CustomError.new(notFound);
     }
   } catch (error) {
-    return res.status(500).json({ error });
+    next(error);
   }
 };
 
-const readProducts = async (req, res) => {
+const readProducts = async (req, res, next) => {
   try {
     const { page } = req.query;
     const all = await read(page);
-    return res.status(200).json({ message: "Read!", response: all });
+    if (all.docs.lenght > 0) {
+      return res.status(200).json({ message: "Read!", response: all });
+    } else {
+      CustomError.new(notFound);
+    }
   } catch (error) {
-    return res.status(500).json({ error });
+    next(error);
   }
 };
 
-const createMockProduct = async (req, res) => {
+const createMockProduct = async (req, res, next) => {
   try {
     const one = await createMock();
     return res.status(201).json({ message: "Created!", response: one });
   } catch (error) {
-    return res.status(500).json({ error });
+    next(error);
   }
 };
 
-const createMockProducts = async (req, res) => {
+const createMockProducts = async (req, res, next) => {
   try {
     const { quantity } = req.params;
     const products = await createMocks(quantity);
     return res.status(201).json({ message: "Created!", response: products });
   } catch (error) {
-    return res.status(500).json({ error });
+    next(error);
   }
 };
 
--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,25 +1,62 @@
-import { response } from "express";
 import User from "../dao/models/user.model.js";
 import { createHashUtil, verifyHashUtil } from "../utils/hash.util.js";
 import { createTokenUtil, verifyTokenUtil } from "../utils/token.util.js";
+import CustomError from "../utils/errors/custom.error.js";
+import { badAuth, forbidden } from "../utils/errors/dictionary.errors.js";
+import { loginService, registerService } from "../services/sessions.service.js";
 
-const register = async (req, res) => {
-  const { email, password, role } = req.body;
-  const error = { error: "Bad auth!" };
-  if (!email || !password) {
-    return res.status(401).json(error);
+const register = async (req, res, next) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      CustomError.new(badAuth);
+    }
+    const one = await User.findOne({ email });
+    if (one) {
+      CustomError.new(badAuth);
+    }
+    const user = await registerService({ email, password, role });
+    const message = "User Registered";
+    return res.status(201).json({ response: user._id, message });
+  } catch (error) {
+    next(error);
   }
-  const one = await User.findOne({ email });
-  if (one) {
-    return res.status(401).json(error);
-  }
-  const hashPassword = createHashUtil(password);
-  const data = { email, password: hashedPassword, role };
-  const user = await User.create(data);
-  const message = "User Registered";
-  return res.status(201).json({ response: user._id, message });
 };
-const login = async (req, res) => {};
-const signout = (req, res) => {};
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      CustomError.new(badAuth);
+    }
+    const one = await User.findOne({ email });
+    if (!one) {
+      CustomError.new(badAuth);
+    }
+    const token = await loginService({ password, one });
+    const opts = { maxAge: 60 * 60 * 24 * 7 * 1000, httpOnly: true };
+    const message = "User logged in!";
+    return res
+      .cookie("token", token, opts)
+      .json({ response: one._id, message });
+  } catch (error) {
+    next(error);
+  }
+};
+const signout = (req, res, next) => {
+  try {
+    const token = req?.cookies?.token;
+    if (!token) {
+      CustomError.new(badAuth);
+    }
+    const verifyToken = verifyTokenUtil(token);
+    if (!verifyToken) {
+      CustomError.new(forbidden);
+    }
+    const message = "User signed out!";
+    return res.clearCookie("token").json({ response: token.user_id, message });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export { register, login, signout };
